f68fd20bc6378915296f7a227a98662585f24f5d
Han shenquan <1173956412@qq.com>
2015-11-07 14:49:24 +0800

    Create ArrayTest.java

---
+++ java-test/ArrayTest.java
@@ -0,0 +1,100 @@
+import java.util.Arrays;
+
+/**
+ * Created by Administrator on 2015/11/6.
+ */
+public class ArrayTest {
+    public static void main(String[] args) {
+//        char []sf={'a','b'};
+//        char [] gg="fge";
+        /*int arr[];
+        arr = new int[5];
+        for (int i : arr) {
+            System.out.println(i);
+        }
+        int month[] = new int[12];*/
+        /*int arr[]=new int[]{1,2,3};
+        int arr2[]={2,3,3};
+//        int size = RamUsageEstimator.humanSizeOf(arr);
+        int day[]=new int[]{31,28,31,30,31,30,31,31,30,31,30,31};
+        for(int i= 0;i<12;i++){
+            System.out.println(i+1+"月有"+day[i]+"天；");
+        }*/
+       /* int a[][];
+        a=new int[2][4];
+        System.out.println(a.length);
+        a=new int[2][];
+        a[0]=new int[2];
+        a[1]=new int[3];*/
+//        int myarr[][]={{12,0},{2,3,4}};
+
+        /*int a[][]=new int[][]{{1,3},{3,5,56}};
+        for(int i=0;i<a.length;i++){
+            for(int j =0;j<a[i].length;j++){
+                System.out.print(a[i][j]+" ");
+            }
+            System.out.println();
+        }*/
+        /*int b[][]=new int[][]{{1},{2,3},{4,5,6}};
+        for(int k=0;k<b.length;k++){
+            for(int c=0;c<b[k].length;c++){
+                System.out.print(b[k][c]);
+            }
+            System.out.println();
+        }*/
+      /*  int[][] arr2 = {{8, 7}, {5, 6}};
+        System.out.println("数组中的元素是：");
+        for (int i = 0; i < arr2.length; i++) {
+//            for (int e : x) {
+            for (int j = 0; j < arr2[i].length; j++) {
+                if (j == arr2[i].length - 1) {
+                    System.out.print(arr2[i][j]+" ");
+                } else {
+                    System.out.print(arr2[i][j] + "、");
+                }
+            }*/
+        /*int arr[]=new int[]{3,4,56,76,7};
+        Arrays.fill(arr,1,2,8);
+        Arrays.sort(arr);
+        for(int x:arr){
+            System.out.println(x);
+
+        }*/
+//        char [] arr = new char[5];
+        /*int arr[]=new int[]{23,42,34};
+        int newarr[]=Arrays.copyOf(arr,5);
+        for(int e:newarr)
+            System.out.println(e);*/
+        /*int arr[] = new int[]{23, 254, 35, 63, 5};
+        int newarr[] = Arrays.copyOfRange(arr, 1, 6);
+        for (int e : newarr)
+            System.out.println(e);*/
+        /*int ia[]=new int[]{1,8,9,4,5};
+        Arrays.sort(ia);
+        int index = Arrays.binarySearch(ia,0);
+        System.out.println("index="+index);
+
+        String str[]=new String[]{"ab","cd","ef","yz"};
+        Arrays.sort(str);
+
+        int index1 = Arrays.binarySearch(str,0,2,"cd");
+        System.out.println("cd的索引位置为："+index1);*/
+       /* int a[] = new int[] {1, 3, 4, 6, 8, 9};
+        int x1 = Arrays.binarySearch(a, 1, 4, 5);
+        int x2 = Arrays.binarySearch(a, 1, 4, 4);
+        int x3 = Arrays.binarySearch(a, 1, 4, 2);
+        int x4 = Arrays.binarySearch(a, 1, 3, 10);
+        System.out.println("x1:" + x1 + ", x2:" + x2);
+        System.out.println("x3:" + x3 + ", x4:" + x4);*/
+        /*int a=-3;
+        int b=4;
+        a=a^b;
+        b=a^b;
+        a=a^b;
+        System.out.println("a="+a+" b="+b);*/
+
+
+
+
+    }
+}
